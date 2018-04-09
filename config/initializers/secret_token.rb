b285f1fd698bb93b5ed09d0f95933bb99adebf04
grid22 <grid22@example.com>
2018-04-09 10:32:26 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestRuby20::Application.config.secret_key_base = '00c40fa0e316ce1e389657cee106ceddedbafeecdb69fecb7dbce05d50c6a9c0fe0b210f26ee93d2ec9f2d6d9e630e8b9496966eaa2b594e4538434174e3eca4'
